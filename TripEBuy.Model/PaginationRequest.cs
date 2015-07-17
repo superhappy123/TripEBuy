ebcd98b58367408b8b68727d6fb45f42ea4bb8a2
superhappy123 <myjn123@163.com>
2015-07-17 14:24:46 +0800

    增加通用查询类

---
+++ TripEBuy.Model/PaginationRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripEBuy.Model
+{
+    public class PaginationRequest
+    {
+        public string Tables { get; set; }
+        public string PrimaryKey { get; set; }
+        public int CurrentPage { get; set; }
+        public string Sort { get; set; }
+        public int PageSize { get; set; }
+        public string Fields { get; set; }
+        public string Filter { get; set; }
+        public string Group { get; set; }
+    }
+}
---
+++ TripEBuy.Model/PaginationResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripEBuy.Model
+{
+    public class PaginationResponse
+    {
+        public DataTable dt { get; set; }
+        public int TotalRecord { get; set; }
+        public int TotalPage { get; set; }
+    }
+}
