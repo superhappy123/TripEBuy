cb0aac29acdebda27b05a0b514600dab9634e49a
superhappy123 <myjn123@163.com>
2015-07-17 13:46:08 +0800

    更改项目引用

---
+++ TripEBuy.Model/UserAccountInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripEBuy.Model
+{
+    public class UserAccountInfo
+    {
+        [Display(Name = "企业会员")]
+        public int CustomerID { get; set; }
+
+        [Display(Name = "企业id")]
+        public int ProtocolCustomerID { get; set; }
+
+        [Display(Name = "企业总部id")]
+        public int AttributeHeadID { get; set; }
+
+        //账号类型  1：订车人 2：分公司账号 3：总部账号
+        public int Accounttpe { get; set; }
+    }
+}
